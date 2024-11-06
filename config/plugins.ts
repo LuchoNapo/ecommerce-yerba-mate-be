0e732fa4410376b1eb17f263b3307c91ccb22c23
Lucho Napo <Luciano_napo@hotmail.com>
2024-11-06 10:11:51 -0300

    [add] cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default ({ env }: { env: (key: string) => string }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('dwuaomsru'),
+          api_key: env('539877535361312'),
+          api_secret: env('Ayu3djUMOF3wyxKsPg1yfhlOh7g'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
